92b679b1d7190081f98cc9a4855ba0f42c182de3
Tachera Sasi <tacherasasi@gmail.com>
2025-08-20 20:17:38 +0300

    fix: Clean up formatting and remove commented-out code in main.go

--- main.go
+++ main.go
@@ -11,22 +11,26 @@ import (
 //go:embed all:frontend/dist
 var assets embed.FS
 
-func init(){
+func init() {
 	//setup auto start on device startup
 }
 
 func main() {
 	app := NewApp()
 
+	// systray := app.NewSystemTray()
+	// systray.SetLabel("My App")
+	// systray.SetIcon(iconBytes)
+
 	err := wails.Run(&options.App{
-		Title:  "HeyMe",
-		Width:  360,
-		Height: 160,
+		Title:         "HeyMe",
+		Width:         360,
+		Height:        160,
 		DisableResize: true,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 250, G: 249, B: 238, A: 0}, // #FAF9EE, transparent
+		BackgroundColour: &options.RGBA{R: 250, G: 249, B: 238, A: 0},
 		OnStartup:        app.startup,
 		Bind: []interface{}{
 			app,
