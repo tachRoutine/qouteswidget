d675b372248bf48bc528eb99bebd6178cc15220e
Tachera Sasi <tacherasasi@gmail.com>
2025-08-20 12:44:59 +0300

    feat: Update application title from "qouteswidget" to "HeyMe"

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "qouteswidget",
+		Title:  "HeyMe",
 		Width:  300,
 		Height: 160,
 		AssetServer: &assetserver.Options{
