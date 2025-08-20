c9b5f65a9eb21eeacd5adfef2b5910f2b23ee1d0
Tachera Sasi <tacherasasi@gmail.com>
2025-08-20 13:29:54 +0300

    feat: Update index.html and app.css for quote card layout and styling

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,19 +3,6 @@ import "./app.css";
 
 import { Greet } from "../wailsjs/go/main/App";
 
-
-document.querySelector("#app").innerHTML = `
-  <div class="quote-card">
-    <div class="result" id="result">Welcome! Get your daily quote 👇</div>
-    <button class="btn" style="margin-top:1.5rem;width:100%;background:#DCCFC0;color:#333;" onclick="getQuote()">Show Daily Quote</button>
-  </div>
-`;
-
-
-let nameElement = document.getElementById("name");
-nameElement.focus();
-let resultElement = document.getElementById("result");
-
 // Setup the getQuote function
 window.getQuote = function () {
   // Call App.GetRandomQuote()
